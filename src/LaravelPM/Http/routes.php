2a43a4e612927dac1d4959c51822ea19f168860d
Nikolaj Petersen <nikolaj.lovenhardt@gmail.com>
2016-01-25 15:27:55 +0100

    Routes

--- src/LaravelPM/Http/routes.php
+++ src/LaravelPM/Http/routes.php
@@ -1,2 +1,12 @@
 <?php
 
+Route::group(['prefix' => 'pm', 'as' => 'pm.'], function () {
+    // Inbox
+    Route::get('/inbox', ['as' => 'inbox', 'uses' => 'LaravelPM\Http\Controllers\PMController@inbox']);
+
+    // Read conversation
+    Route::get('/read/{conversation}', ['as' => 'read', 'uses' => 'LaravelPM\Http\Controllers\PMController@read']);
+
+    // Compose
+    Route::any('/compose', ['as' => 'compose', 'uses' => 'LaravelPM\Http\Controllers\PMController@compose']);
+});
